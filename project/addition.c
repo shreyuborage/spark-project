bf67a66408eef1936288011143d89595d8d352c8
shreyuborage <112372660+shreyuborage@users.noreply.github.com>
2022-08-29 11:30:34 +0530

    2nd commit

---
+++ project/addition.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+main()
+{
+	int a,b,c;
+	printf("\n enter the value of a=");
+	scanf("%d",&a);
+	printf("\n enter the value of b=");
+	scanf("%d",& b);
+	c=a+b;
+	printf("\n add=%d",c);
+}
---
+++ project/circle.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+main()
+{
+	int r;
+	float pi=3.14,area;
+	printf("\n enter the value of radius=");
+	scanf("%d",&r);
+	area=pi*r*r;
+	printf("\n area of circle=%f",area);	
+}
